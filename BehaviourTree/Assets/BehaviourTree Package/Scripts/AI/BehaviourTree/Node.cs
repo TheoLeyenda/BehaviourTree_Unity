eb9427cdeeb25fd0424bc67cd865ec483d0e00db
Theo Leyenda <theoleyenda1997@gmail.com>
2022-08-19 11:48:22 -0300

    Remove Usings

--- BehaviourTree/Assets/BehaviourTree Package/Scripts/AI/BehaviourTree/Node.cs
+++ BehaviourTree/Assets/BehaviourTree Package/Scripts/AI/BehaviourTree/Node.cs
@@ -1,10 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Jobs;
-using UnityEngine.Jobs;
-using Unity.Collections;
-using Unity.Burst;
 
 namespace BehaviorTree
 {
